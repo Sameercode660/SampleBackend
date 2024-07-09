f6d9a11d46bfa016385e9ac081e7c554bef2784b
Mohd Sameer <mesh789736@gmail.com>
2024-07-09 16:04:01 +0530

    add api route

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ app.get('/', (req, res) => {
     return res.status(200).json({message: 'Server is successfully responding'})
 })
 
+app.get('/api', (req, res) => {
+    return res.status(200).json({message: 'This is a normal message from another route'})
+})
+
 app.listen(process.env.PORT, () => {
     console.log('Server is listening on PORT', process.env.PORT)
 })
